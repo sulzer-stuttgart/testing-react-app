c2f7a07debff38e55e35785696875dc6b488f480
Mustafa Masetic <mustafa.masetic@sulzer.de>
2020-08-23 15:50:13 +0200

    Add cypress with test coverage

---
+++ cypress/integration/login.spec.js
@@ -0,0 +1,32 @@
+describe('Registratioin', () => {
+  
+  it('Failed login without password and email typing', () => {
+    cy.visit('http://localhost:3000/')
+    cy.get('.button')
+      .click()
+    cy.scrollTo('top')
+  })
+
+  it('Failed login', () => {
+    cy.visit('http://localhost:3000/')
+    cy.get('#email')
+      .type('mustafa@sulzer.de')
+    cy.get('#password')
+      .type('ksjdfhkjdfsdf')
+    cy.get('.button')
+      .click()
+    cy.scrollTo('top')
+  })
+
+  it('Successful login', () => {
+    cy.visit('http://localhost:3000/')
+    cy.get('#email')
+      .type('mustafa@sulzer.de')
+    cy.get('#password')
+      .type('Password1!')
+    cy.get('.button')
+      .click()
+    cy.scrollTo('top')
+  })
+
+})
